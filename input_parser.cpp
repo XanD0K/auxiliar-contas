f4c985157feb01c068116115bb678a2a7b956eae
Pedro <pedrop6@hotmail.com.br>
2022-09-15 00:02:34 -0700

    briefs

--- input_parser.cpp
+++ input_parser.cpp
@@ -3,10 +3,15 @@
 //
 
 #include "input_parser.hpp"
+
+/// Constructor, parses the input arguments and stores them in a vector of strings
 InputParser::InputParser(int &argc, char **argv) {
 	for(int i = 1; i < argc; ++i) tokens.emplace_back(argv[i]);
 }
 
+/// \brief Get the value of a command line option
+/// \param option The FULL option to get the value of
+/// \return The value of the option if it exists, otherwise an empty string
 const std::string &InputParser::getCmdOption(const std::string &option) const {
 	static const std::string empty_string;
 
@@ -14,6 +19,15 @@ const std::string &InputParser::getCmdOption(const std::string &option) const {
 	if(itr != tokens.end() && ++itr != tokens.end()) return *itr;
 	return empty_string;
 }
-bool InputParser::cmdOptionExists(const std::string &option) const {
-	return std::find(tokens.begin(), tokens.end(), option) != tokens.end();
+
+/// \brief Check if a command line option exists
+/// \param optionSmall The SHORT option to check for
+/// \param optionFull The FULL option to check for
+/// \return True if the option exists, otherwise false
+bool InputParser::cmdOptionExists(const std::string &optionSmall,
+								  const std::string &optionFull) const {
+	bool exists = false;
+	exists = std::find(tokens.begin(), tokens.end(), optionFull) != tokens.end();
+	exists |= std::find(tokens.begin(), tokens.end(), optionSmall) != tokens.end();
+	return exists;
 }
--- input_parser.hpp
+++ input_parser.hpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <vector>
 
+/// \brief Class to parse command line arguments
 class InputParser {
 	std::vector<std::string> tokens{};
 
@@ -16,5 +17,6 @@ public:
 
 	[[nodiscard]] const std::string &getCmdOption(const std::string &option) const;
 
-	[[nodiscard]] bool cmdOptionExists(const std::string &option) const;
+	[[nodiscard]] bool cmdOptionExists(const std::string &optionSmall,
+						 const std::string &optionFull) const;
 };
