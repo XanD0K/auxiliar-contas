2faef8ca1125b7cd7d8e71ffaa36fff4beb30da3
Pedro <pedrop6@hotmail.com.br>
2022-09-19 21:35:23 -0700

    small fix

--- input_parser.cpp
+++ input_parser.cpp
@@ -10,7 +10,7 @@ InputParser::InputParser(int &argc, char **argv) {
 }
 
 /// \brief Get the value of a command line option
-/// \param option The FULL option to get the value of
+/// \param option FULL option to get the value of
 /// \return The value of the option if it exists, otherwise an empty string
 const std::string &InputParser::getCmdOption(const std::string &option) const {
 	static const std::string empty_string;
@@ -22,7 +22,7 @@ const std::string &InputParser::getCmdOption(const std::string &option) const {
 
 /// \brief Check if a command line option exists
 /// \param optionFull The FULL option to check for
-/// \param optionSmall The SHORT option to check for
+/// \param optionSmall The SHORT option to check for (optional)
 /// \return True if the option exists, otherwise false
 bool InputParser::cmdOptionExists(const std::string &optionFull,
 								  const std::string &optionSmall) const {
--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 
 /// \brief constants for command line options and program VERSION
 namespace constants {
-	constexpr auto HELP = "--HELP";
+	constexpr auto HELP = "--help";
 	constexpr auto HELP_SHORT = "-h";
-	constexpr auto VERSION = "--VERSION";
+	constexpr auto VERSION = "--version";
 	constexpr auto VERSION_SHORT = "-v";
 
 	constexpr uint VERSION_MAJOR = 0;
@@ -29,17 +29,17 @@ int main(int argc, char *argv[]) {
 
 	InputParser input(argc, argv);
 
-	if(input.cmdOptionExists(HELP_SHORT, HELP)) {
+	if(input.cmdOptionExists(HELP, HELP_SHORT)) {
 		std::cout << "Aux Bills Help" << '\n';
 		std::cout << "Usage: aux-bill [options]" << '\n';
 		std::cout << "Options:" << '\n';
-		std::cout << "  -h, --HELP\t\t\tShow this HELP message and exit" << '\n';
-		std::cout << "  -v, --VERSION\t\t\tShow program's VERSION number and exit"
+		std::cout << "  -h, --help\t\t\tShow this HELP message and exit" << '\n';
+		std::cout << "  -v, --version\t\t\tShow program's VERSION number and exit"
 				  << '\n';
 		exit(EXIT_SUCCESS);
 	}
 
-	if(input.cmdOptionExists(VERSION_SHORT, VERSION)) {
+	if(input.cmdOptionExists(VERSION, VERSION_SHORT)) {
 		std::cout << "Aux Bills Version: " << VERSION_MAJOR << "." << VERSION_MINOR << "."
 				  << VERSION_PATCH << '\n';
 		exit(EXIT_SUCCESS);
